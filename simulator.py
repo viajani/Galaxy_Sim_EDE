9f781154fd2526e555730c83edf485431496f017
mShuntov <33184615+mShuntov@users.noreply.github.com>
2020-08-25 11:57:29 +0200

    Update simulator.py
    Saves stamps of ~23000 galaxies in a ~0.1 deg^2 patch in a .npy file ready to be easily used in the ML algo as X data (same .npy format as used in the colab example). Also saves the galaxy parameters which will the labels Y in the ML algo.

--- simulator.py
+++ simulator.py
@@ -24,7 +24,7 @@ Short description
 import sys
 import os
 import math
-import numpy as np
+import numpy
 import logging
 import time
 import galsim
@@ -53,7 +53,7 @@ def patch_selection(cat, patchsize):
     patches = []
     for i in range(len(grid_ra)-1):
         for j in range(len(grid_dec)-1):
-            cond = (cat['RA_MAG'] > grid_ra[i]) & (cat['RA_MAG'] < grid_ra[i+1]) & (cat['DEC_MAG'] > grid_dec[j]) & (cat['DEC_MAG'] < grid_dec[j+1])   
+            cond = (cat['RA_MAG'] > grid_ra[i]) & (cat['RA_MAG'] < grid_ra[i+1]) & (cat['DEC_MAG'] > grid_dec[j]) & (cat['DEC_MAG'] < grid_dec[j+1]) # & (cat['VIS'] < 21)          
             patches.append(cat[cond])
     
     return patches
@@ -89,7 +89,7 @@ def main(argv):
 #     cat_data=cat_table.to_pandas()
     
     # slicing the catalogue into patches
-    patchsize = 1/60 #(size of the patch is 1x1 arcmin)
+    patchsize = 20/60 #(size of the patch is 1x1 arcmin)
     patches = patch_selection(cat_table, patchsize)
 
     # work with one patch at a time
@@ -100,13 +100,14 @@ def main(argv):
     rhDall=[]
     rhBall=[]
     nsersicall=[]
+    axisratioall=[]
     ell1all=[]
     ell2all=[]
     shear1all=[]
     shear2all=[]
     diskangleall=[]
 
-    # work with one patch at a time, store the needed quantities for every patch in a list
+    # work with one patch at a time
     for patch in patches:
         nobj.append(len(patch))
 
@@ -115,8 +116,9 @@ def main(argv):
         magall.append(patch['VIS'])
         rhDall.append(patch['DISK_LENGTH'])
         rhBall.append(patch['BULGE_LENGTH'])
-        # nsersicall at the moment is not read. it will be assigned either 1 or 4 if galaxy is disk or elliptical
+        # nsersicll at the moment is not read. it will be assigned either 1 or 4 if galaxy is disk or elliptical
         # not sure if 'DISK_ANGLE' is in fact the fi angle that computes the ellipticity compontents
+        axisratioall.append(patch['DISK_AXIS_RATIO'])
         ell1, ell2 = get_ell_12(patch['DISK_AXIS_RATIO'], patch['DISK_ANGLE'])
         ell1all.append(ell1)
         ell2all.append(ell2)
@@ -124,39 +126,6 @@ def main(argv):
         shear2all.append(patch['GAMMA2'])
         diskangleall.append(patch['DISK_ANGLE'])
     
-    xsize = 128                      # pixels
-    ysize = 128                      # pixels
-    
-
-######
-    
-#     shear1=np.random.uniform(-0.05, 0.05,1)
-#     shear2=np.random.uniform(-0.05, 0.05,1)
-    
-    
-# ###DEFINE IMAGE PARAMETERS###
-
-#     image_prop_path=argv[2]
-#     image_prop_table=Table.read(image_prop_path, format='fits')
-#     image_data=image_prop_table.to_pandas()
-
-#     random_seed = 8241574
-
-#     pixel_scale = image_data.pixel_scale               # arcsec / pixel  (size units in input catalog are pixels)
-#     image_size = image_data.image_size               # pixels
-
-#     t_exp = image_data.t_exp #s
-#     gain = image_data.gain #e-/ADU
-#     readoutnoise = image_data.readoutnoise #e-
-#     sky_bkg = image_data.sky_bkg #mag/arcsec2
-    
-#     ZP=image_data.ZP #mag
-
-#     F_sky = pixel_scale**(2)*t_exp*10**(-(sky_bkg-ZP)/2.5) #e-/pixel
-#     noise_variance = ( numpy.sqrt( ( (readoutnoise)**2 + F_sky ) ) *1/gain )**2 #e- -> ADU by dividing sigma by gain ; sigma = 4.9ADU
-######
-
-
 ###DEFINE IMAGE PARAMETERS###
     num = argv[1] #number to appear in the image name
 
@@ -165,7 +134,7 @@ def main(argv):
     pixel_scale = 0.1               # arcsec / pixel  (size units in input catalog are pixels)
     xsize = 128                      # pixels
     ysize = 128                      # pixels
-    image_size = np.int(60/0.1)               # pixels
+    image_size = np.int(62*22/0.1)               # pixels
 
     t_exp = 3*565 #s
     gain = 3.1 #e-/ADU
@@ -175,11 +144,9 @@ def main(argv):
     ZP=24.0 #mag
 
     F_sky = pixel_scale**(2)*t_exp*10**(-(sky_bkg-ZP)/2.5) #e-/pixel
-    noise_variance = ( np.sqrt( ( (readoutnoise)**2 + F_sky ) ) *1/gain )**2 #e- -> ADU by dividing sigma by gain ; sigma = 4.9ADU
+    noise_variance = ( numpy.sqrt( ( (readoutnoise)**2 + F_sky ) ) *1/gain )**2 #e- -> ADU by dividing sigma by gain ; sigma = 4.9ADU
 ######
 
-
-   
 
 ###DISPLAY IMAGE INFO###
     logger.info('\nStarting simulator using:')
@@ -191,7 +158,7 @@ def main(argv):
     
     logger.info('\n    - Sky background = %.2f mag/arcsec2', sky_bkg)
     logger.info('    - Read-out noise = %.1f e-', readoutnoise)
-    logger.info('    - Gaussian noise (sigma = %.2f ADU/pixel)', np.sqrt(noise_variance))
+    logger.info('    - Gaussian noise (sigma = %.2f ADU/pixel)', numpy.sqrt(noise_variance))
 
     logger.info('\n    - Airy PSF (lam=600,700,800, diam=1.2, obscuration=0.3)')
     logger.info('    - Sersic galaxies')
@@ -208,10 +175,10 @@ def main(argv):
     #dudy = -math.sin(theta.rad()) * pixel_scale
     #dvdx = math.sin(theta.rad()) * pixel_scale
     #dvdy = math.cos(theta.rad()) * pixel_scale
-    dudx = np.cos(theta) * pixel_scale
-    dudy = -np.sin(theta) * pixel_scale
-    dvdx = np.sin(theta) * pixel_scale
-    dvdy = np.cos(theta) * pixel_scale
+    dudx = numpy.cos(theta) * pixel_scale
+    dudy = -numpy.sin(theta) * pixel_scale
+    dvdx = numpy.sin(theta) * pixel_scale
+    dvdy = numpy.cos(theta) * pixel_scale
 
 #     image_center = full_image.true_center
 #     affine = galsim.AffineTransform(dudx, dudy, dvdx, dvdy, origin=full_image.true_center)
@@ -261,8 +228,8 @@ def main(argv):
     logger.info('\n\nStarting to simulate galaxies')
 
     # go over the patches
-    for p in range(len(patches)):    
-#     for p in range(5):    
+#     for p in range(len(patches)):    
+    for p in range(1):    
         ###CREATE OUTPUT IMAGES###    
         file_name ='output/sim_patch-%s_nonoise.fits' %(p)
         file_name_noise ='output/sim_patch-%s_noise.fits' %(p)
@@ -273,6 +240,7 @@ def main(argv):
         ra_cent = np.mean(RAall[p])
         dec_cent = np.mean(DECall[p])
         sky_center = galsim.CelestialCoord(ra=ra_cent*galsim.degrees, dec=dec_cent*galsim.degrees)
+        print('\n sky center', sky_center)
         wcs = galsim.TanWCS(affine, sky_center, units=galsim.arcsec)
         full_image.wcs = wcs
         logger.info('Image %r and %r created',file_name,file_name_noise)
@@ -280,22 +248,27 @@ def main(argv):
         # at the moment the center of the image and its wcs are the same for each patch. Realistically this should change
         ######
 
+        stamps_noise=[]
+        stamps_nonoise=[]
+        galaxy_params=[]
         for k in range(nobj[p]):
+#         for k in range(300):
             #Read galaxy parameters from catalog
             RA = RAall[p][k]
             DEC = DECall[p][k]
             # from RA,DEC get pixel position on the image
             world_pos = galsim.CelestialCoord(RA*galsim.degrees, DEC*galsim.degrees)
             image_pos = wcs.toImage(world_pos)
-            
+                   
             ## if disk galaxy, rh is the disk length and sersic is 1
             ## if elliptical galaxy, rh is bulge lenght and sersic is 4
-            if rhDall != 0:
-                half_light_radius = rhDall[p][k]
-                nsersic=1
-            else:
-                half_light_radius = rhBall[p][k]
+            axisratio = axisratioall[p][k]
+            if rhDall == 0:
+                half_light_radius = rhBall[p][k]*np.sqrt(axisratio) #* 3.459
                 nsersic=4
+            else:
+                half_light_radius = rhDall[p][k]*np.sqrt(axisratio) * 1.7
+                nsersic=1
                 
             mag = magall[p][k]
             ell1 = ell1all[p][k]
@@ -305,16 +278,17 @@ def main(argv):
             shear2 = np.mean(shear2all[p])
             diskangle = diskangleall[p][k]
                         
-            ### DISPLAY INFO FOR GALAXIES IN PATCH###
-            logger.info('    - Patch %.0f', p)
-            logger.info('    - Galaxy %.0f', k)
-            logger.info('    - position RA,DEC %.3f,%.3f', RA, DEC)
-            logger.info('    - position X,Y %.0f,%.0f', image_pos.x, image_pos.y)
-            logger.info('    - magnitude %.2f', mag)
-            logger.info('    - half-light radius %.2f', half_light_radius)
-            logger.info('    - sersic index %.2f', nsersic)
-            logger.info('    - ellipticity %.4f,%.4f\n', ell1,ell2)
-
+#             ### DISPLAY INFO FOR GALAXIES IN PATCH###
+#             logger.info('\n   - world_pos', world_pos)
+#             logger.info('     -image_pos', image_pos)
+#             logger.info('    - Patch', p)
+#             logger.info('    - Galaxy ', k)
+#             logger.info('    - position RA,DEC %.3f,%.3f', RA, DEC)
+#             logger.info('    - magnitude %.2f', mag)
+#             logger.info('    - half-light radius %.2f', half_light_radius)
+#             logger.info('    - sersic index', nsersic)
+#             logger.info('    - ellipticity %.4f,%.4f', ell1,ell2)
+#             logger.info('    - shear %.4f,%.4f\n', shear1,shear2)
 
             #Galaxy is a sersic profile:
             fluxflux = t_exp/gain*10**(-(mag-ZP)/2.5)
@@ -322,13 +296,13 @@ def main(argv):
             gal = gal.shear(e1=ell1, e2=ell2)
             gal = gal.shear(g1=shear1, g2=shear2)
 
-
             #Rotate galaxy
             ang = diskangle
             gal = gal.rotate(theta=ang*galsim.degrees)
 
             #convolve galaxy with PSF
             final = galsim.Convolve([psf, gal])
+#             final = gal
 
             #offset the center for pixelization (of random fraction of half a pixel)
             ud = galsim.UniformDeviate(random_seed+k)
@@ -344,11 +318,32 @@ def main(argv):
             image = galsim.ImageF(xsize,ysize,scale=pixel_scale)
             final.drawImage(image=image,wcs=wcs.local(image_pos), offset=offset)
             image.setCenter(ix_nominal,iy_nominal)
-
+            
             #add stamps to single image
             bounds = image.bounds & full_image.bounds
             full_image[bounds] += image[bounds]
-
+            
+            # save the stamps of each galaxy with no noise first
+            stamps_path = 'output/stamps/galaxy_ns'+str(nsersic)+'_stamp_'+str(p)+'-'+str(k)
+#             image.write(stamps_path+'_nonoise.fits')
+            stamps_nonoise.append(image)
+    
+            # ## add noise
+            rng_gal = galsim.BaseDeviate(random_seed)
+            noise_gal = galsim.GaussianNoise(rng_gal, sigma=math.sqrt(noise_variance))
+            image_noise = image
+            image_noise.addNoise(noise_gal)
+#             image_noise.write(stamps_path+'_noise.fits')
+            stamps_noise.append(image_noise)
+    
+            # save the parameters of the galaxies
+            galaxy_params.append(np.array([ell1, ell2, shear1, shear2, 
+                                           mag, half_light_radius, axisratio, nsersic, diskangle]))
+            
+        np.save('output/galaxy_stamps_nonoise_p'+str(p)+'.npy', stamps_nonoise)
+        np.save('output/galaxy_stamps_noise_p'+str(p)+'.npy', stamps_noise)
+        np.save('output/galaxy_params_p'+str(p)+'.npy', galaxy_params)
+            
         timegal = time.time()
         logger.info('%d galaxies computed in t=%.2f s',k+1,timegal-timeigal)
     ######
